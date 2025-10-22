18015ce7fbd233809a78c4ff21bb4d78adfa810c
EL FELLAH Meryem <elfellahmeryem2@gmail.com>
2025-10-22 11:09:58 +0100

    FC

---
+++ src/main/java/net/meryem/dao/DaoImpl.java
@@ -0,0 +1,11 @@
+package net.meryem.dao;
+
+public class DaoImpl implements IDao {
+
+    @Override
+    public double getData() {
+        System.out.println("Version base de donnees");
+        double t = 34;
+        return t;
+    }
+}
---
+++ src/main/java/net/meryem/dao/IDao.java
@@ -0,0 +1,5 @@
+package net.meryem.dao;
+
+public interface IDao {
+    double getData();
+}
