890fc2fad3bcd784333e3eb2f187426458d862a2
EL FELLAH Meryem <elfellahmeryem2@gmail.com>
2025-11-05 01:04:43 +0100

    On a cree une autre classe Pres22 pour essayer encore avec l'instanciation dynamique mais cette fois ci en donnant la main a l'utilisateur a choisir quelle version il veut utiliser, donc sans utilisation/lecture du fichier config.txt

--- src/main/java/net/meryem/pres/Pres22.java
+++ src/main/java/net/meryem/pres/Pres22.java
@@ -1,27 +1,23 @@
 package net.meryem.pres;
+
 import net.meryem.dao.IDao;
 import net.meryem.metier.IMetier;
+
+import java.io.File;
 import java.lang.reflect.Method;
 import java.util.Scanner;
-import java.io.File;
+
 import static java.lang.Class.forName;
 
-public class Pres2 {
+public class Pres22 {
     public static void main(String[] args) throws Exception {
-        //DaoImpl dao = new DaoImpl();
-        Scanner sc = new Scanner(new File("config.txt"));
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Donner la classe de la couche dao: ");
         String daoClassName = sc.nextLine();
         Class cDao = forName(daoClassName);
         IDao dao = (IDao) cDao.getConstructor().newInstance(); //new Dao implementation (couplage faible)
-        System.out.println(dao.getData());
-        /*
-        * maitenant on a assure que aussi cette classe depend juste des interfaces, puis grace a l'instanciation
-        * dynamique(config.txt), on a rendu aussi cette classe Presentation fermee a la modif et ouverte a l'ext
-        * */
-        /*
-        * Par la suite on veut utiliser la methode calcul du MetierImpl, ou on aura besoin de faire "Injection des dependances",
-        * mais d''une maniere dynamique
-        * */
+
+        System.out.println("Donner la classe de la couche metier: ");
         String metierClassName = sc.nextLine();
         Class cMetier = forName(metierClassName);
         IMetier metier =  (IMetier) cMetier.getConstructor().newInstance();
